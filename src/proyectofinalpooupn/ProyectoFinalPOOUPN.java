77f5a12c80706f9a6e74e78e630abdc7fe5d0a63
Sebas <CytroX567@gmail.com>
2026-09-13 22:29:12 -0500

    Actualiza proyecto CRUD

--- src/proyectofinalpooupn/ProyectoFinalPOOUPN.java
+++ src/proyectofinalpooupn/ProyectoFinalPOOUPN.java
@@ -15,6 +15,7 @@ public class ProyectoFinalPOOUPN {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola, este es mi proyecto CRUD");
     }
     
 }
