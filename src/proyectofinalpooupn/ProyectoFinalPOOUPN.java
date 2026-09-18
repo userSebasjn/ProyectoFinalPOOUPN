2159db4fd20587ebc58a4cadd44fa8150af88776
Sebas <CytroX567@gmail.com>
2026-09-17 21:33:53 -0500

    Cambio en rama de colaboración

--- src/proyectofinalpooupn/ProyectoFinalPOOUPN.java
+++ src/proyectofinalpooupn/ProyectoFinalPOOUPN.java
@@ -7,6 +7,7 @@ public class ProyectoFinalPOOUPN {
     public static void main(String[] args) {
         System.out.println("Hola, este es mi proyecto CRUD");
         System.out.println("Cambio realizado después de resolver el conflicto");
+        System.out.println("Cambio realizado en la rama de colaboración");
     }
     
 }
