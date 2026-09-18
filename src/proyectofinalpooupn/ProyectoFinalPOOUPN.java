6500f0959b92a1aef11ac2fa7e4c5523d4ee55b9
charlymori99-creator <charlymori99@gmail.com>
2026-09-17 20:41:13 -0500

    Cambio realizado en semana2

--- src/proyectofinalpooupn/ProyectoFinalPOOUPN.java
+++ src/proyectofinalpooupn/ProyectoFinalPOOUPN.java
@@ -1,21 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package proyectofinalpooupn;
 
-/**
- *
- * @author USUARIO
- */
+
 public class ProyectoFinalPOOUPN {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Hola, este es mi proyecto CRUD");
+        System.out.println("Cambio realizado en la rama semana2");
     }
     
 }
